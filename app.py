eaace988258636488924476c77933d36e6274ef7
Vamika Arya <144547966+6vam4arya@users.noreply.github.com>
2024-12-08 12:53:50 +0000

    Adding Files

--- app.py
+++ app.py
@@ -1,26 +1,11 @@
-#Vamika Arya
-from flask import Flask, redirect, url_for, request
-import csv
-file=open("form.csv","w")
+from flask import Flask,request,render_template
 app=Flask(__name__)
-#First Binding for greeting the user
-@app.route("/<user>")
-def greet(user):
-    return f'<html><body><h1><style="color:pink">Welcome {user}</h1></body></html>'
-#Second Binding for handling the requests
-@app.route('/store data',methods=["POST","GET"])
-def handling_data():
-    if request.method=="POST":
-        user_name=request.form["user"]
-        writer=csv.writer(file)
-        writer.writerows(user_name)
-        return redirect(url_for('greet',user=user_name))
-    else:
-        user=request.args.get["user"]
-        return redirect(url_for(greet,user=user))
-if (__name__=="__main__"):
+
+@app.route('/storedata',methods=['POST'])
+def store_data():
+    if request.method=='POST':
+        form_dict=request.form
+        return render_template('store_data_app.html',dict=form_dict)
+
+if __name__=="__main__":
     app.run(debug=True)
-
-
-        
-    
